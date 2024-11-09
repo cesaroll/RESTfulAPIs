1437dfabe39967571853dbf89bb55028fa0e96eb
cl352v_wayfair <clopezlerma@wayfair.com>
2024-11-09 21:59:18 +0100

    Creating a temporary Db

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,9 +1,13 @@
+using Movies.App;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
