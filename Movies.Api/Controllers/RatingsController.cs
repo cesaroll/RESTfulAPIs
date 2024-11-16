4ec14a7e13a873f9ad2a8a491353dbb4ab46dd05
cl352v_wayfair <clopezlerma@wayfair.com>
2024-11-16 23:34:42 +0100

    Rating a Movie

---
+++ Movies.Api/Controllers/RatingsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Auth;
+using Movies.App.Services;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+public class RatingsController : ControllerBase
+{
+    private readonly IMovieRatingsService _movieRatingsService;
+    private readonly AuthContext _authContext;
+
+    private readonly ILogger<RatingsController> _logger;
+
+    public RatingsController(IMovieRatingsService movieRatingsService, ILogger<RatingsController> logger, AuthContext authContext)
+    {
+        _movieRatingsService = movieRatingsService;
+        _logger = logger;
+        _authContext = authContext;
+    }
+
+    [Authorize]
+    [HttpPut(ApiEndpoints.Movies.Rate)]
+    public async Task<IActionResult> Rate(
+        [FromRoute] Guid id,
+        [FromBody] RateMovieRequest request,
+        CancellationToken token
+    )
+    {
+        Guid userId = _authContext.UserId!.Value;
+
+        var result = await _movieRatingsService.RateMovieAsync(id, userId, request.Rating, token);
+
+        return result.Match<IActionResult>(
+            success => success ? Ok() : NotFound(),
+            error => BadRequest(error)
+        );
+    }
+}
--- Movies.Db/Entities/RatingEntity.cs
+++ Movies.Db/Entities/RatingEntity.cs
@@ -1,12 +1,19 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Movies.Db.Entities;
 
 public class RatingEntity
 {
+  [Required]
   public Guid UserId { get; set; }
+
+  [Required]
   public Guid MovieId { get; set; }
+
+  [Required]
   public int Rating { get; set; }
 
-  public required MovieEntity Movie { get; set; }
+  [Required]
+    public MovieEntity Movie { get; set; }
 }
