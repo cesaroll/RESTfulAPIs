525a2b3c4d593d24bec2f531ff3996ab29dea15c
cl352v_wayfair <clopezlerma@wayfair.com>
2024-12-15 14:05:05 +0100

    Ysing HttpClientFactory

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,10 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
 using Movies.Api.Sdk;
 using Movies.Contracts.Requests;
 using Refit;
 
-var moviesApi = RestService.For<IMoviesApi>("http://localhost:5149");
+
+// var moviesApi = RestService.For<IMoviesApi>("http://localhost:5149");
+
+var services = new ServiceCollection();
+
+
+services.AddRefitClient<IMoviesApi>()
+    .ConfigureHttpClient(x =>
+        x.BaseAddress = new Uri("http://localhost:5149")
+    );
+
+var provider = services.BuildServiceProvider();
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
 
 
 Console.WriteLine("\nReading Movie:\n");
