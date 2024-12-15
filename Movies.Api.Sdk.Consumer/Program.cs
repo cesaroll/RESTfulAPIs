832a641b51c8a3bf344286750df962d41d108dc3
cl352v_wayfair <clopezlerma@wayfair.com>
2024-12-15 13:56:41 +0100

    Retrieve movies using Refit

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text.Json;
 using Movies.Api.Sdk;
+using Movies.Contracts.Requests;
 using Refit;
 
 var moviesApi = RestService.For<IMoviesApi>("http://localhost:5149");
@@ -10,5 +11,19 @@ Console.WriteLine("\nReading Movie:\n");
 var movie = await moviesApi.GetMoviesAsync("f7f5bde4-5d02-4c5e-85d8-e2adadb4bbc5", default);
 
 Console.WriteLine(JsonSerializer.Serialize(movie, new JsonSerializerOptions{
+    WriteIndented = true
+}));
+
+
+Console.WriteLine("\nGet All Movies:\n");
+var request = new GetAllMoviesRequest
+{
+    Page = 1,
+    PageSize = 10
+};
+
+var movies = await moviesApi.GetAllMoviesAsync(request, default);
+
+Console.WriteLine(JsonSerializer.Serialize(movies, new JsonSerializerOptions{
     WriteIndented = true
 }));
--- Movies.Api.Sdk/IMoviesApi.cs
+++ Movies.Api.Sdk/IMoviesApi.cs
@@ -1,3 +1,4 @@
+using Movies.Contracts.Requests;
 using Movies.Contracts.Responses;
 using Refit;
 
@@ -6,5 +7,8 @@ namespace Movies.Api.Sdk;
 public interface IMoviesApi
 {
     [Get(ApiEndpoints.V2.Movies.Get)]
-    Task<MovieResponse> GetMoviesAsync(string idOrSlug, CancellationToken token);
+    Task<MovieResponse> GetMoviesAsync(string idOrSlug, CancellationToken token = default);
+
+    [Get(ApiEndpoints.V1.Movies.GetAll)]
+    Task<MoviesResponse> GetAllMoviesAsync(GetAllMoviesRequest request, CancellationToken token = default);
 }
